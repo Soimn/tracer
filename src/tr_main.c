ee6c12955c0d1ef5f45196125cac8b3e89d62c18
Soimn <soimn.dok@gmail.com>
2022-01-31 16:10:25 +0100

    fixed double hit and added resolution ramp

--- src/tr_main.c
+++ src/tr_main.c
@@ -34,6 +34,9 @@ typedef struct Camera
     u32* data;
     u32 width;
     u32 height;
+    u32 target_width;
+    u32 target_height;
+    u32 fragment_size;
     V3 pos;
     V4 rot;
     f32 fov;
@@ -50,7 +53,7 @@ typedef struct Hit_Data
 } Hit_Data;
 
 internal Hit_Data
-CastRay(Entity* entities, umm entity_count, V3 origin, V3 dir)
+CastRay(Entity* entities, umm entity_count, Hit_Data* prev_hit, V3 origin, V3 dir)
 {
     Hit_Data hit_data = {0};
     
@@ -74,12 +77,12 @@ CastRay(Entity* entities, umm entity_count, V3 origin, V3 dir)
                     V3 hit          = V3_Scale(dir, t);
                     f32 hit_dist_sq = V3_LengthSq(hit);
                     
-                    if (hit_dist_sq < closest_hit_dist_sq)
+                    if (hit_dist_sq < closest_hit_dist_sq && (prev_hit == 0 || prev_hit->entity != &entities[i]))
                     {
                         V3 normal = V3_Normalize(V3_Sub(hit, pos));
                         
                         hit_data.entity     = &entities[i];
-                        hit_data.point      = V3_Scale(dir, t - 0.0001f); // HACK: to avoid hit points inside spheres
+                        hit_data.point      = hit;
                         hit_data.normal     = normal;
                         hit_data.reflection = V3_Normalize(V3_Add(dir, V3_Scale(normal, -V3_Inner(dir, normal))));
                         closest_hit_dist_sq = hit_dist_sq;
@@ -104,10 +107,10 @@ CastRay(Entity* entities, umm entity_count, V3 origin, V3 dir)
                     V3 hit          = V3_Scale(dir, t);
                     f32 hit_dist_sq = V3_LengthSq(hit);
                     
-                    if (hit_dist_sq < closest_hit_dist_sq)
+                    if (hit_dist_sq < closest_hit_dist_sq && (prev_hit == 0 || prev_hit->entity != &entities[i]))
                     {
                         hit_data.entity     = &entities[i];
-                        hit_data.point      = V3_Scale(dir, t - 0.0001f); // HACK: to avoid hit points through planes
+                        hit_data.point      = hit;
                         hit_data.normal     = normal;
                         hit_data.reflection = V3_Normalize(V3_Add(dir, V3_Scale(normal, -V3_Inner(dir, normal))));
                         closest_hit_dist_sq = hit_dist_sq;
@@ -135,21 +138,18 @@ TraceScene(Camera camera, Entity* entities, umm entity_count)
     f32 cw = near_plane_width / camera.width;
     f32 ch = near_plane_height / camera.height;
     
-    static f32 SUN = 0;
-    V3 sun_dir = V3_Normalize(Vec3(Sin(SUN), 1, Cos(SUN)));
-    //V3 sun_dir = Vec3(0, 1, 0);
-    SUN += Platform->dt;
+    V3 sun_dir = V3_Normalize(Vec3(Sin(40), 1, Cos(40)));
     
     for (umm cy = 0; cy < camera.height; ++cy)
     {
         for (umm cx = 0; cx < camera.width; ++cx)
         {
-            V3 cell = { cx * cw, -(cy * ch), 0 };
+            V3 cell = { (cx + 0.5f) * cw, -((cy + 0.5f) * ch), 0 };
             
-            Hit_Data hit_data    = CastRay(entities, entity_count, camera.pos, V3_Normalize(V3_Add(co, cell)));
+            Hit_Data hit_data    = CastRay(entities, entity_count, 0, camera.pos, V3_Normalize(V3_Add(co, cell)));
             Hit_Data bounce_data = {0};
             
-            if (hit_data.entity != 0) bounce_data = CastRay(entities, entity_count, hit_data.point, sun_dir);
+            if (hit_data.entity != 0) bounce_data = CastRay(entities, entity_count, &hit_data, hit_data.point, sun_dir);
             
             umm bounce_count = (hit_data.entity != 0) + (bounce_data.entity != 0);
             
@@ -162,33 +162,42 @@ TraceScene(Camera camera, Entity* entities, umm entity_count)
             camera.data[cy * camera.width + cx] = color;
 #endif
             
+            V3 color = camera.sky_color;
             if (hit_data.entity != 0)
             {
                 f32 l = V3_Inner(hit_data.normal, sun_dir);
                 
-                V3 color = V3_FromRGBU32(hit_data.entity->color);
+                color = V3_FromRGBU32(hit_data.entity->color);
                 
                 color = V3_Scale(color, MIN(MAX(0.2f, l), 1));
                 
                 color = V3_Scale(color, 1.0f / bounce_count);
-                
-                Platform->image[cy * Platform->width + cx] = V3_ToRGBU32(color);
             }
             
-            else Platform->image[cy * Platform->width + cx] = V3_ToRGBU32(camera.sky_color);
+            u32 fragment_width  = camera.target_width / camera.width;
+            u32 fragment_height = camera.target_height / camera.height;
+            for (umm j = 0; j < fragment_height; ++j)
+            {
+                for (umm i = 0; i < fragment_width; ++i)
+                {
+                    camera.data[(cy * fragment_height + j) * camera.target_width + (cx * fragment_width + i)] = V3_ToRGBU32(color);
+                }
+            }
         }
     }
 }
 
 global Camera MainCamera = {
-    .data   = 0,
-    .width  = 0,
-    .height = 0,
-    .pos    = { 0, 0, 0},
-    .rot    = { 0, 0, 0, 1 },
-    .fov    = HALF_PI32,
-    .bounce_count = 1,
-    .sky_color    = {0.1f, 0.5f, 0.9f},
+    .data          = 0,
+    .width         = 0,
+    .height        = 0,
+    .target_width  = 0,
+    .target_height = 0,
+    .pos           = { 0, 0, 0},
+    .rot           = { 0, 0, 0, 1 },
+    .fov           = HALF_PI32,
+    .bounce_count  = 1,
+    .sky_color     = {0.1f, 0.5f, 0.9f},
 };
 
 global Entity Entities[] = {
@@ -211,12 +220,22 @@ Tick(Platform_Data* platform_data)
 {
     Platform = platform_data;
     
-    //if (MainCamera.data == 0 || MainCamera.width != Platform->width || MainCamera.height != Platform->height)
+    if (MainCamera.target_width != Platform->width ||
+        MainCamera.target_height != Platform->height)
     {
-        MainCamera.data   = Platform->image;
-        MainCamera.width  = Platform->width;
-        MainCamera.height = Platform->height;
+        MainCamera.target_width  = Platform->width;
+        MainCamera.target_height = Platform->height;
+        MainCamera.fragment_size = 512;
+    }
+    
+    if (MainCamera.width != MainCamera.target_width ||
+        MainCamera.height != MainCamera.target_height)
+    {
+        MainCamera.width  = (u32)((f32)MainCamera.target_width  / MainCamera.fragment_size); 
+        MainCamera.height = (u32)((f32)MainCamera.target_height / MainCamera.fragment_size); 
+        MainCamera.fragment_size = MAX(MainCamera.fragment_size / 2, 1);
         
+        MainCamera.data = Platform->image;
         TraceScene(MainCamera, Entities, ARRAY_SIZE(Entities));
         Platform->SwapBuffers();
     }
